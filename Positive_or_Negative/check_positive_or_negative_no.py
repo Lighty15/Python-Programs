df20636a3b23e59200ca46c9696389941988c5d0
Lighty15 <162425592+Lighty15@users.noreply.github.com>
2024-03-15 21:06:06 +0530

    Add files via upload

---
+++ Positive_or_Negative/check_positive_or_negative_no.py
@@ -0,0 +1,9 @@
+#Python Program to check given number is positive or negative
+
+n=int(input("Enter a number:"))
+
+if(n>=0):
+    print(n,"is positive number")
+    
+else:
+    print(n,"is negative number")
