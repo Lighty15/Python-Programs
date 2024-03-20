87954907f40a66c404493c02fde766aef3be0597
Lighty15 <162425592+Lighty15@users.noreply.github.com>
2024-03-20 21:51:18 +0530

    Add files via upload

---
+++ Maximum of two numbers/maximum_of_two_no.py
@@ -0,0 +1,8 @@
+#Python Program that returns the maximum of two numbers
+
+def maximum(num1,num2):
+    return max(num1,num2)
+
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+print("Maximum of two numbers:",maximum(num1,num2))
