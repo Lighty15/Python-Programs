cadf67c5c84451a2e09a1a4e366e2653775d7dde
Lighty15 <162425592+Lighty15@users.noreply.github.com>
2024-03-27 23:10:29 +0530

    Add files via upload

---
+++ Replace the word with symbol/Replace_word_with_symbol.py
@@ -0,0 +1,7 @@
+#Replace the word with given symbol
+
+str1=input("Enter a string:")
+word_to_replace=input("Enter the word to replace:")
+symbol=input("Enter the symbol:")
+result=str1.replace(word_to_replace,symbol)
+print(result)
