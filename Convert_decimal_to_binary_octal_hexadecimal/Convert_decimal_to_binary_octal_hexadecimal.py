01875bfe9eb637bd99917b7fa2cdcb5c1d9f0822
Lighty15 <162425592+Lighty15@users.noreply.github.com>
2024-03-30 16:39:04 +0530

    Add files via upload

---
+++ Convert_decimal_to_binary_octal_hexadecimal/Convert_decimal_to_binary_octal_hexadecimal.py
@@ -0,0 +1,7 @@
+#Convert decimal number to binary,octal and hexadecimal
+
+num=int(input("Enter a decimal number:"))
+
+print("Binary number:",bin(num))
+print("Octal number:",oct(num))
+print("Hexadecimal number:",hex(num))
