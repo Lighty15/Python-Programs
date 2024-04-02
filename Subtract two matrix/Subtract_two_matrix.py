388a1843dc4b0363cdd20ab643d2aa7ccfb5b9b9
Lighty15 <162425592+Lighty15@users.noreply.github.com>
2024-04-02 08:04:44 +0530

    Add files via upload

---
+++ Subtract two matrix/Subtract_two_matrix.py
@@ -0,0 +1,16 @@
+#Subtracting two matrix
+
+A=[[2,3,4],[3,4,6],[4,2,5]]
+B=[[3,2,6],[2,1,4],[1,4,2]]
+Result=[[0,0,0],[0,0,0],[0,0,0]]
+
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        Result[i][j]=A[i][j]-B[i][j]
+
+print("Matrix A:",A)
+print("Matrix B:",B)
+print("Subtraction of matrices A & B:",)
+
+for r in Result:
+    print(r)
